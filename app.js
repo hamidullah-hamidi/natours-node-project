b7b615ea4966faa55170e75bd8e5fd476108fb9c
Hamidullah <hamidullahh754@gmail.com>
2025-01-20 19:28:50 +0430

    u

--- app.js
+++ app.js
@@ -28,12 +28,48 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => (el.id = id));
+
+  if (id > tours.length || !tours) {
+    return res.status(404).json({
+      status: 'faild',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(
+    { id: newId },
+    req.body
+  ); /* آیدی را با آبچکت درخواست یکجا میکند*/
 
-  res.send('Done');
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        staus: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
