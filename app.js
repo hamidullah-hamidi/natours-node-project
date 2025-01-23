37a2bdcc2ad1c4bf47b60b88bed90e68e651e814
Hamidullah <hamidullahh754@gmail.com>
2025-01-23 10:21:51 +0430

    express.static middleware

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -11,6 +10,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hell from the meddleware');
