3b67c31a180590aaeee1c08a7a34abc130c16a75
Hamidullah <hamidullahh754@gmail.com>
2025-01-23 11:34:55 +0430

    envirnment-variables

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
