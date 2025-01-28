82bbdc9120e7ef2e05c0337fc35f1505b89e9a8f
Hamidullah <hamidullahh754@gmail.com>
2025-01-28 12:06:59 +0430

    refactoring the code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,4 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`tour id is ${val}`);
-
-  if (req.params.id > tours.length || !tours) {
-    res.status(404).json({
-      status: 'faild',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -29,47 +16,33 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     requestedTime: req.requestTime,
     status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
+    // result: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((e) => e.id === id);
+  // const tour = tours.find((e) => e.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tour,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  // result: tours.length,
+  // data: {
+  //   tour,
+  // },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign(
-    { id: newId },
-    req.body
-  ); /* آیدی را با آبچکت delete درخواست  یکجا میکند*/
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        staus: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    staus: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
