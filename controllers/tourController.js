f461c99afbe5dd5adb3d6a2e60d225fbc4410fb6
Hamidullah <hamidullahh754@gmail.com>
2025-02-02 15:33:44 +0430

    make pagination filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,6 +33,13 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 1.5  Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
     // 2) Execute query
     const tours = await query;
 
