69a362b6d15e9ab9a55733d93d23bf4b665b0475
Hamidullah <hamidullahh754@gmail.com>
2025-02-03 15:10:11 +0430

    getTourStats

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -114,6 +114,27 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStates = async () => {
   try {
+    const stats = Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
   } catch (err) {
     res.status(404).json({
       status: 'faild',
