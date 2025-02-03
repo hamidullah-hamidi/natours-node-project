f544278e9c258246302bf3d1733a5bd446b06fe9
Hamidullah <hamidullahh754@gmail.com>
2025-02-03 10:37:36 +0430

    limit method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,13 +14,12 @@ class APIFeatures {
   }
 
   filter() {
-    // 1.1) filtering
     const queryObj = { ...queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 1.2) Advanced  filtering
+    //  Advanced  filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
@@ -31,12 +30,22 @@ class APIFeatures {
   }
 
   sort() {
-    // 1.3  Sorting
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
     }
 
     return this;
@@ -68,13 +77,13 @@ exports.getAllTours = async (req, res) => {
     //   query = query.sort('-createdAt');
     // }
 
-    // 1.4  Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
+    // // 1.4  Field limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v');
+    // }
 
     // 1.5  Pagination
     const page = req.query.page * 1 || 1;
