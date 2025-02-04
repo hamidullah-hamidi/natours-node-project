a704e23720e08ab9bc559d97367580416eaf1734
Hamidullah <hamidullahh754@gmail.com>
2025-02-04 15:24:05 +0430

    u

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { default: slugify } = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour mast have a duration'],
@@ -65,8 +67,9 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 //  Document meddleware: runs before .save() and .create()
-tourSchema.pre('save', function () {
-  console.log(this);
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
