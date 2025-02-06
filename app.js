26effc0cdf362edda45d4a2a45ca5aaa037b9852
Hamidullah <hamidullahh754@gmail.com>
2025-02-06 12:21:33 +0430

    u

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utilities/appError');
 
 const app = express();
 
@@ -31,23 +32,17 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'faild',
-  //   message: `Can't find ${req.originalUrl} in this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} in this server!`);
-  (err.status = 'fail'), (err.statusCode = 404);
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} in this server!`));
 });
 
 // Error handling meddleware
 app.use((err, req, res, next) => {
+  console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  req.status(err.statusCode).json({
+  res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
