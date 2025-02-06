a569f7305999e926bd093b3570a991325ee074ad
Hamidullah <hamidullahh754@gmail.com>
2025-02-06 11:17:35 +0430

    create ERROR

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// 2) Routs
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
 app.all('*', (req, res, next) => {
   // res.status(404).json({
   //   status: 'faild',
@@ -40,15 +45,11 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
 });
-
-// 2) Routs
-
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
 
 module.exports = app;
