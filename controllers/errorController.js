c65c204484ae3f5ff53b56885c77a5ecdb38673c
Hamidullah <hamidullahh754@gmail.com>
2025-02-06 15:37:24 +0430

    u

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,28 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd();
+  }
 };
