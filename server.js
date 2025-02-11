0f53d2a6ef098e4fa8de052f3cc52b97e4f2ec35
Hamid <hamidullahh754@gmail.com>
2025-02-11 14:40:40 -0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -17,26 +24,18 @@ mongoose
   })
   .then((con) => {
     console.log(con.connections);
-    console.log('Db connection successfull!');
+    console.log('Db connection successful!');
   });
 
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
-  console.log(`App runing on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
-  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNCAUGHT EXCEPTIOIN! 💥 Shutting down...');
+  console.log('UNHANDIER REJECTION! 💥 Shutting down...');
   server.close(() => {
     process.exit(1);
   });
