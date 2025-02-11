50d47c607398eb4915d1328d4fffd4f40ec9e8bd
Hamid <hamidullahh754@gmail.com>
2025-02-11 09:53:53 -0800

    u

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllUsers = (req, res) => {
+const User = require('../models/userModel');
+const catchAsync = require('../utilities/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: users,
+  });
+
   res.status(500).json({
     status: 'error',
     message: 'The route is not defined yet',
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
