e5cf4501a7368d33f878429e84fc47e140e2b850
Hamid <hamidullahh754@gmail.com>
2025-02-11 14:50:18 -0800

    make user model

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have a email'],
+  },
+  photo: {
+    type: String,
+    required: [true, 'A user must have a photo'],
+  },
+  password: {
+    type: Number,
+    required: [true, 'A user must have a password'],
+  },
+  passwordConfirm: {
+    type: Number,
+    required: [true, 'A user must have a passwordConfirm'],
+  },
+});
+
+const userModel = mongoose.Model('userModel', userSchema);
