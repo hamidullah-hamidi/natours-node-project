17e5f14b1949239af06c9c6d6fa1a601af434f37
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-12 04:04:52 -0800

    u

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,14 +35,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function (next) {
-  // Only run this function if password was actually modified
+userSchema.pre('save', function (next) {
   if (!this.isModified('password')) return next;
 
-  // Hash the password with cost of 12
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = bcrypt.hash(this.password, 12);
 
-  // Delete the passwordConfirm field
   this.passwordConfirm = undefined;
 
   next();
