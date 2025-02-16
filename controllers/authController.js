d31ed5f29d5579cafe1e973ca9da62e520fb33ad
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-16 15:10:17 +0430

    JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,15 @@
 const jwt = require('jsonwebtoken');
+const AppError = require('../utilities/AppError');
 const User = require('../models/userModel');
 const catchAsync = require('../utilities/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -17,3 +23,22 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1)  Check if email && password exist
+  if (!password && !email) {
+    next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2)  Check if user exist && password is correct
+  // 3)  if everything is ok, send token to client
+
+  const token = '';
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
