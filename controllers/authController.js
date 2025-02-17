227401070e81571162aafdf7108a0d7811f5a72f
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-17 14:48:31 +0430

    jwt.verify

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,37 +57,26 @@ exports.protect = catchAsync(async (req, res, next) => {
   // 1)  Get the token and check if it's true
 
   let token;
-  if (req.headers.authorization && req.headers.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     token = req.headers.authorization.split(' ')[1];
   }
   console.log(token);
 
   if (!token) {
     return next(
-      new AppError('You are not logged in, please login to get access!', 401)
+      new AppError('You are not logged in!, please login to get access.', 401)
     );
   }
 
   // 2)  Verification token
-  jwt.verify(token, process.env.JWT_SECRET);
+  const decode = await promisify(jwt.verify(token, process.env.JWT_SECRET));
+  console.log(decode);
 
   // 3)  Check if user still exists
   // 4)  Check if user changed password after the token was issued
 
   next();
 });
-
-let token;
-if (
-  req.headers.authorization &&
-  req.headers.authorization.startsWith('Bearer')
-) {
-  token = req.headers.authorization.split(' ')[1];
-}
-console.log(token);
-
-if (!token) {
-  return next(
-    new AppError('You are not logged in!, please login to get access.', 401)
-  );
-}
