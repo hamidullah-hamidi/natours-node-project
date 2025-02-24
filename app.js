e9c93f8c7774a8bdea388627c521c849bf206911
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-24 17:37:20 +0430

    install XSS and sanitize

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const { default: helmet } = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQl query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
