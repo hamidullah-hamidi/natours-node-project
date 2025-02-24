ddd443e1e3a01d4a7391a3cddd6722fc99d63c3f
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-24 17:11:07 +0430

    u

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
-const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const { default: helmet } = require('helmet');
 
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const AppError = require('./utils/appError');
 
 const app = express();
 
 // 1) MIDDLEWARES
-console.log(process.env.NODE_ENV);
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'To many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   console.log('Hell from the middleware');
   next();
--- controllers/authController.js
+++ controllers/authController.js
@@ -15,16 +15,19 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieExpiresIn = process.env.JWT_COOKIE_EXPIRES_IN || 7;
+  const expiresDate = new Date(
+    Date.now() + cookieExpiresIn * 24 * 60 * 60 * 1000
+  );
+
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: expiresDate,
     httpOnly: true,
   };
-
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
+
   user.password = undefined;
 
   res.status(statusCode).json({
