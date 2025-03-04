5205432c8d597358b207e44d36f32d9b8f72f323
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-03-04 21:38:33 +0430

    create review controller and routes

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,31 +1,37 @@
 const { default: mongoose, mongo } = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'Review con not be empty!'],
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review con not be empty!'],
+    },
+    ratting: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Tour must have a rating!'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a Tour!'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a User!'],
+    },
   },
-  ratting: {
-    type: Number,
-    min: 1,
-    max: 5,
-    required: [true, 'Tour must have a rating!'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  tour: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Tour',
-    required: [true, 'Tour must have a reference!'],
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Tour must have a reference!'],
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 
