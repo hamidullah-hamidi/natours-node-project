f6021dc322188de3e38d55ca25124143ff6d9319
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-03-04 22:14:00 +0430

    u

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -13,7 +13,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (res, res, next) => {
+exports.createReview = catchAsync(async (req, res, next) => {
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
--- routes/reviewRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const router = express.Router();
-
-router
-  .route('/')
-  .get(reviewController.getAllReviews)
-  .post(reviewController.createReview);
