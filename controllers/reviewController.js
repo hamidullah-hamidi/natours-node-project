82950a2941ce9b6c501931f1d22aa441d286bd68
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-03-04 23:01:52 +0430

    add populate

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,9 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  const reviews = await Review.find().populate({
+    path:
+  });
 
   res.status(200).json({
     status: 'success',
