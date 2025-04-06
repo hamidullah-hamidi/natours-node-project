ab499d7421cf51a097103a4dd9cbdfa623f29b0d
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-04-06 16:31:33 +0430

    add a factory function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Review.find();
@@ -23,3 +24,5 @@ exports.createReview = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -13,4 +13,6 @@ router
   )
   .post(reviewController.createReview);
 
+router.route('/:id').delete(reviewController.deleteReview);
+
 module.exports = router;
