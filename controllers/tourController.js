4ba8d961b1ead6bcead25aec8be6645f5dcb36ba
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-04-14 22:00:15 +0430

    Completed this section

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -101,9 +101,11 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
 // '/tours-within/:distance/center/:latlng/unit/:unit',
 // '/tours-within/233/center/34.111745,-118.113491/unit/mi',
 
-exports.getToursWithin = (req, res, next) => {
+exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
 
   if (!lat || !lng) {
     return new AppError(
@@ -112,9 +114,58 @@ exports.getToursWithin = (req, res, next) => {
     );
   }
 
-  console.log(distance, lat, lng, unit);
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
+      },
+    },
+  });
 
   res.status(200).json({
     status: 'success',
+    data: {
+      tours,
+    },
   });
-};
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multipLier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    return new AppError(
+      'Please provide Latitude or Longitude in format lat, lng',
+      400
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinate: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multipLier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      distances,
+    },
+  });
+});
