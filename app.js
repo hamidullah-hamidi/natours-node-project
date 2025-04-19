bdca3b7752bbd69d2be7ed24e5a0ce381b83afa4
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-04-19 09:59:03 +0430

    recreating project or cloning

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -72,6 +73,7 @@ app.use((req, res, next) => {
 
 // 2) Routs
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
