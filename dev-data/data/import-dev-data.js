3fbbdb71bc457e0679984213a9706e78e8190821
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-08-23 09:27:35 +0430

    u

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -21,15 +21,15 @@ mongoose
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-);
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await User.create(users, { validationBeforeSave: false });
+    await User.create(users, {
+      validationBeforeSave: false,
+    }); /* Validation on the model well be false */
     await Review.create(reviews);
 
     console.log('Data successfully loaded!');
